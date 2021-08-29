409b072ec40b4eb12bc95c876bbed6afa4e30328
wan921 <ewang2@levi.com>
2021-08-28 18:08:40 -0700

    json update

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,34 @@
 <script>
-	export let name;
-</script>
+	import { LayerCake, Svg } from "layercake";
+	import { data } from "./data/data";
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
+  
+  </script>
+  
+  <main>
+	<div class="chart-container">
+	  <!-- <LayerCake
+		data={grouped}
+		padding={{ top: 0, right: 0, bottom: 0, left: 0 }}
+	  >
+		<Svg>
+		  <Treemap bind:hovered {formatDollars} {rootHeight} />
+		</Svg>
+		<Tooltip {hovered} {formatDollars} />
+	  </LayerCake> -->
+	</div>
+  </main>
+  
+  <style>
+  
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	  width: 100%;
+	  height: 100%;
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+  
+	.chart-container {
+	  width: 100%;
+	  height: 100%;
 	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+  </style>
+  
